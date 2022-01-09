01cf90de05cf2f3d4e4f1fcb8b2e95a6a9e05355
MISSIC <prajjwalt9@gmail.com>
2022-01-09 08:25:16 +0545

    The shortening

--- index.js
+++ index.js
@@ -7,30 +7,24 @@ const secHand = document.querySelector(".second-hand");
 const hourHand = document.querySelector(".hour-hand");
 const minHand = document.querySelector(".minute-hand");
 
-secHand.style.transform = `translateY(-50%) rotateZ(${
-  (currSec / 60) * 360
-}deg)`;
-hourHand.style.transform = `translateY(-50%) rotateZ(${
-  (currHour / 12 +currSec/3600 + currMin/60) * 360
-}deg)`;
-minHand.style.transform = `translateY(-50%) rotateZ(${
-  (currMin / 60) * 360
-}deg)`;
 
-let change = setInterval(() => {
+const changeTime = () => {
   let intervalTime = new Date();
   let hours =
     intervalTime.getHours() > 12
       ? intervalTime.getHours() - 12
       : intervalTime.getHours();
- 
-    secHand.style.transform = `translateY(-50%) rotateZ(${
-      (intervalTime.getSeconds() / 60) * 360
-    }deg)`;  
+
+  secHand.style.transform = `translateY(-50%) rotateZ(${
+    (intervalTime.getSeconds() / 60) * 360
+  }deg)`;
   hourHand.style.transform = `translateY(-50%) rotateZ(${
-    (hours / 12 + intervalTime.getMinutes()/3600) * 360
+    (hours / 12 + intervalTime.getMinutes() / 3600) * 360
   }deg)`;
   minHand.style.transform = `translateY(-50%) rotateZ(${
-    (intervalTime.getMinutes() / 61 + intervalTime.getSeconds()/3660) * 360
+    (intervalTime.getMinutes() / 61 + intervalTime.getSeconds() / 3660) * 360
   }deg)`;
-}, 100);
+};
+
+window.onload = changeTime;
+setInterval(changeTime, 100);
